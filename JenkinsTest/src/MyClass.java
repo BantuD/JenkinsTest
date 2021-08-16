0a62964b64af53716f9ad4b20ceab535a5cbd9d2
Rajul Dubey <46771371+BantuD@users.noreply.github.com>
2021-08-16 15:59:58 +0530

    Updated the code

--- JenkinsTest/src/MyClass.java
+++ JenkinsTest/src/MyClass.java
@@ -3,5 +3,7 @@ public class MyClass {
 	public static void main(String arg[])
 	{
 		System.out.println("This is bantu");
+		System.out.println("This is another line of code");
+		System.out.println("this is also another line of code);
 	}
 }
